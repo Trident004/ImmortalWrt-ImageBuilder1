9309b71a8ae5f142f8d6d0b5730ddad112bb61ab
ShimizuKawasaki <xuehangzhou2011@gmail.com>
2026-06-29 20:26:00 +0800

    Clean up APK links and package declarations
    Removed unnecessary APK download links and updated package list.

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -45,12 +45,8 @@ mkdir -p /home/build/immortalwrt/packages/
 # 定义你的专属 APK 下载直链
 CUSTOM_APKS=(
     "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/luci-app-quickstart-0.12.7-r1.apk"
-    "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/luci-app-store-0.2.0-r3.apk"
     "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/luci-i18n-quickstart-zh-cn-26.176.34044.f2b69d3.apk"
-    "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/luci-lib-taskd-1.0.25.apk"
-    "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/luci-lib-xterm-4.18.0.apk"
     "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/quickstart-0.13.0-r1.apk"
-    "https://github.com/ShimizuKawasaki/nas-packages-luci-actions/releases/download/auto-build-28368922170-1/taskd-1.0.3-r2.apk"
 )
 
 # 循环下载并存放到本地 packages 索引目录中
@@ -85,7 +81,7 @@ PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 
 # ================== 🌟 声明打包自定义 APK 🌟 ==================
 # 将刚才下载的组件名称显式加入打包列表，ImageBuilder 会自动从本地 /packages 目录读取
-PACKAGES="$PACKAGES luci-app-quickstart luci-app-store luci-i18n-quickstart-zh-cn luci-lib-taskd luci-lib-xterm quickstart taskd"
+PACKAGES="$PACKAGES luci-app-quickstart luci-i18n-quickstart-zh-cn quickstart"
 
 # 强烈建议在 25.12 环境下补充 luci-compat（LuCI 旧版兼容层），确保 iStore 前端不报错
 PACKAGES="$PACKAGES luci-compat"
